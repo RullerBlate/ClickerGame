c6823b2aecbd95ac462b17d204b284f6296b7f37
RullerBlate <115470262+RullerBlate@users.noreply.github.com>
2022-12-20 13:05:01 +0300

    Summary

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using TMPro;
+
+public class Coin : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _moneyShower;
+    [SerializeField] private TMP_Text _moneyPerSecondShower;
+
+    public float Money { get; private set; }
+
+    private float _addingMoneyEverySecond;
+    private float _addingMoneyEveryClick = 1f;   
+    private int _delay = 1;
+    private float _lastMoneyIncrease;
+
+    public void ClickCoin()
+    {
+        Money += _addingMoneyEveryClick;
+        _moneyShower.text = Money.ToString();
+    }
+
+    private void Update()
+    {
+        if (_lastMoneyIncrease <= 0)
+        {
+            Money += _addingMoneyEverySecond;
+            Money = Convert.ToSingle(Math.Round(Money, 1));
+            _lastMoneyIncrease = _delay;
+            _moneyShower.text = Money.ToString();
+        }
+        
+        _lastMoneyIncrease-= Time.deltaTime;
+    }
+
+    public void IncreaseMoneyEverySecond(float increasingMoney)
+    {
+        _addingMoneyEverySecond += increasingMoney;
+        _moneyPerSecondShower.text = _addingMoneyEverySecond.ToString();
+    }
+
+    public void BuyUpgrade(float Price)
+    {
+        Money -= Price;
+        Money = Convert.ToSingle(Math.Round(Money, 1));
+        _moneyShower.text = Money.ToString();
+    }
+}
---
+++ Assets/Scripts/IncreasingMoney.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using TMPro;
+
+public class IncreasingMoney : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _priceShower;
+    [SerializeField] private Coin _coin;
+    [SerializeField] private float _price;
+    [SerializeField] private float _increasingMoney;
+
+    public void TryingBuyUpgrade()
+    {
+        if (_coin.Money >= _price)
+        {
+            _coin.BuyUpgrade(_price);
+            _coin.IncreaseMoneyEverySecond(_increasingMoney);
+            _price *= 1.5f;
+            _price = Convert.ToSingle(Math.Round(_price));
+            _priceShower.text = _price.ToString();
+        }
+    }
+}
